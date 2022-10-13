d8c3b522c07638a13c35e57534d45b65202f37a0
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-13 02:53:46 -0300

    chore: setup tokens to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,25 @@ module.exports = {
     "./src/**/*.tsx"
   ],
   theme: {
+    fontSize: {
+      xs: 12,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+      '3xl': 48,
+    },
+    colors: {
+      black: '#000',
+      white: '#fff',
+      purple: {
+        100: '#E7D7F0',
+        400: '#675C87',
+        700: '#563373',
+        800: '#381B50'
+      }
+    },
     extend: {
       fontFamily: {
         sans: 'Montserrat, sans-serif'
