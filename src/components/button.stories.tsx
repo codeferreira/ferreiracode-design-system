57c2c6fa227e129a07343d52e2093bc13babe31d
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-14 02:29:00 -0300

    feat: update button component to receive a icon

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,22 +1,67 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps } from './button';
+import { Download } from 'phosphor-react';
+import { Button, ButtonRootProps } from './button';
 
 export default {
   title: 'Components/Button',
-  component: Button,
+  component: Button.Root,
   args: {
-    children: 'Get in touch',
+    variant: 'default',
+    children: (
+      [
+        'Get in touch'
+      ]
+    ),
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+    variant: {
+      options: ['default', 'outline', 'naked'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
   }
-} as Meta<ButtonProps>
+} as Meta<ButtonRootProps>
 
-export const Default: StoryObj<ButtonProps> = {}
-export const Outlined: StoryObj<ButtonProps> = {
+export const Default: StoryObj<ButtonRootProps> = {}
+
+export const Outlined: StoryObj<ButtonRootProps> = {
   args: {
     variant: 'outline'
   }
 }
-export const Naked: StoryObj<ButtonProps> = {
+
+export const Naked: StoryObj<ButtonRootProps> = {
   args: {
     variant: 'naked'
+  }
+}
+
+export const WithIconLeft: StoryObj<ButtonRootProps> = {
+  args: {
+    variant: 'naked',
+    children: [
+      <Button.Icon>
+        <Download />
+      </Button.Icon>,
+      'Download'
+    ]
+  }
+}
+
+export const WithIconRight: StoryObj<ButtonRootProps> = {
+  args: {
+    variant: 'naked',
+    children: [
+      'Download',
+      <Button.Icon>
+        <Download />
+      </Button.Icon>
+    ]
   }
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,20 +2,35 @@ import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 import { ReactNode } from 'react';
 
-export interface ButtonProps {
+
+export interface ButtonIconProps {
+  children: ReactNode;
+}
+
+function ButtonIcon({children}: ButtonIconProps) {
+  return (
+    <Slot className="w-6 h-6">
+      {children}
+    </Slot>
+  )
+}
+
+ButtonIcon.displayName = 'Button.Icon'
+
+export interface ButtonRootProps {
   children: ReactNode;
   variant?: 'default' | 'outline' | 'naked';
   asChild?: boolean;
 }
 
-export function Button({ variant = 'default', children, asChild = false }: ButtonProps) {
+function ButtonRoot({ variant = 'default', children, asChild = false }: ButtonRootProps) {
   const Component = asChild ? Slot : 'button'
 
   return (
     <Component
       className={
         clsx(
-          'py-3 px-4 font-semibold rounded-md text-sm w-full transition-colors hover:bg-purple-800 hover:text-white focus:ring-2 ring-purple-100',
+          'py-3 px-4 font-semibold rounded-md text-sm w-full transition-colors hover:bg-purple-800 hover:text-white focus:ring-2 ring-purple-100 flex items-center justify-center gap-3',
           variant === 'default' && 'bg-purple-700 text-white',
           variant === 'outline' && 'bg-transparent text-purple-700 border border-purple-700',
           variant === 'naked' && 'bg-transparent text-purple-700 hover:bg-transparent hover:text-purple-800'
@@ -25,4 +40,11 @@ export function Button({ variant = 'default', children, asChild = false }: Butto
       {children}
     </Component>
   )
+}
+
+ButtonRoot.displayName = 'Button.Root'
+
+export const Button = {
+  Root: ButtonRoot,
+  Icon: ButtonIcon
 }
